4894432ea26da0bdb61fe403e2f3979ac674a851
mdazam26 <azam456alam@gmail.com>
2025-02-01 01:20:02 +0530

    first

---
+++ backend/src/controllers/auth.conroller.js
@@ -0,0 +1,64 @@
+// import { generateToken } from "../lib/utils.js";
+// import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async (req,res) =>{
+    // const {fullName,email,password} = req.body
+    // try {
+    //     // hash password
+    //      if(password.lenght < 6){
+    //         return (res.status(400).json({
+    //             message: "Password must be at least 6 characters "
+    //         }));
+    //      }
+
+    //      const user  = await User.findOne({email})
+
+    //      if (user) return res.status(400).json({
+    //         message : "Email already exists"
+    //      });
+
+    //      const salt = await bcrypt.genSalt(10)
+
+    //      const hashedPassword = await bcrypt.hash(password,salt);
+
+    //      const newUser = new User({
+    //         fullName,
+    //         email,
+    //         password:hashedPassword
+    //      })
+
+    //      if(newUser){
+    //         generateToken(newUser._id,res)
+    //         await newUser.save();
+
+    //         res.status(201).json({
+    //             _id : newUser._id,
+    //             fullName : newUser.fullName,
+    //             email: newUser.email,
+    //             profilePic: newUser.profilePic,
+    //         });
+    //      }
+    //      else{
+    //         res.status(400).json({
+    //             message:"invalid user data"
+    //         });
+    //      }
+
+    // } catch (error) {
+    //     console.log("Error is signup controller ",error.message);
+    //     res.status(500).json({
+    //         message: "Internal Server Error"
+    //     });
+    // }
+
+    res.send("signup router");
+}
+
+export const login = (req,res) =>{
+    res.send("login router");
+}
+
+export const logout = (req,res) =>{
+    res.send("logout router");
+}
