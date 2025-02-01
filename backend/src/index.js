4a03662579b8d41d3390c1f27ca1e9323db156d4
mdazam26 <azam456alam@gmail.com>
2025-02-01 21:05:31 +0530

    backend partially complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 // const express = require("express");
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
@@ -12,7 +13,9 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on port: "+PORT);
