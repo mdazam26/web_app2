ded9fa8c96bc84747a798f06eac291155b4e8070
mdazam26 <azam456alam@gmail.com>
2025-02-01 19:49:00 +0530

    auth added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { login, logout, signup } from "../controllers/auth.conroller.js"
+import { login, logout, signup,updateProfile,checkAuth } from "../controllers/auth.conroller.js"
+import { protectRoute  } from "../middleware/auth.middleware.js"
 
 
 const router = express.Router()
@@ -9,5 +10,10 @@ router.post("/signup",signup)
 router.post("/login" , login)
 
 router.post("/logout",logout)
+
+router.put("/update-profile",protectRoute,updateProfile);
+
+
+router.get("/check",protectRoute,checkAuth)
 
 export default router;
